cc2abbb66bf2f32128c7f5fd730d74fb5c1670b9
prodip-shadow <prodiphore2005@gmail.com>
2026-04-10 22:38:00 +0600

    update import quotes and restructure layout component

--- app/(mainLayout)/layout.tsx
+++ app/(mainLayout)/layout.tsx
@@ -1,6 +1,6 @@
-import Footer from "@/components/MainComponents/Footer/Footer";
-import Navbar from "@/components/MainComponents/Navbar/Navbar";
-import React from "react";
+import Footer from '@/components/MainComponents/Footer/Footer';
+import Navbar from '@/components/MainComponents/Navbar/Navbar';
+import React from 'react';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,17 +8,15 @@ interface LayoutProps {
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
-    <html lang="en">
-      <body className="min-h-screen flex flex-col">
-        {/* Navbar */}
-        <Navbar />
+    <div className="min-h-screen flex flex-col">
+      {/* Navbar */}
+      <Navbar />
 
-        {/* Main Content */}
-        <main className="flex-1">{children}</main>
+      {/* Main Content */}
+      <main className="flex-1">{children}</main>
 
-        {/* Footer */}
-        <Footer />
-      </body>
-    </html>
+      {/* Footer */}
+      <Footer />
+    </div>
   );
 }
