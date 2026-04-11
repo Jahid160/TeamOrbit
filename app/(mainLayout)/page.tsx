476621ba6c22e92a66945639eafbf606744eb498
china akther <chinaakther05@gmail.com>
2026-04-11 14:12:00 +0600

    hero section added

--- app/(mainLayout)/page.tsx
+++ app/(mainLayout)/page.tsx
@@ -1,7 +1,10 @@
+import Hero from "@/components/home/Hero";
 import React from "react";
 
 const homePage = () => {
-  return <div>homePage</div>;
+  return <div className="px-4 md:px-8">
+    <Hero></Hero>
+    </div>;
 };
 
 export default homePage;
