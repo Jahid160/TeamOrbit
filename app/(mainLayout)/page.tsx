42bbad2fafd066ce3e8f7dba4f5e8b2e04b82c68
china akther <chinaakther05@gmail.com>
2026-04-11 14:42:53 +0600

    update home page

--- app/(mainLayout)/page.tsx
+++ app/(mainLayout)/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/home/Hero";
 import React from "react";
 
-const homePage = () => {
-  return <div className="px-4 md:px-8">
-    <Hero></Hero>
-    </div>;
+const HomePage = () => {
+  return (
+    <div className="px-4 md:px-8">
+      <Hero />
+    </div>
+  );
 };
 
-export default homePage;
+export default HomePage;
